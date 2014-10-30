fa2b652c006b7a53c86353a6f7b3149d4cfa5eb8
Doug Hellmann <doug@doughellmann.com>
2014-10-30 11:18:28 -0400

    run db migrations on startup

--- aeromancer/cli/app.py
+++ aeromancer/cli/app.py
@@ -5,10 +5,14 @@ from cliff.app import App
 from cliff.commandmanager import CommandManager
 import pkg_resources
 
+from aeromancer.db import migrations
+
 
 class Aeromancer(App):
 
     log = logging.getLogger(__name__)
+    # CONSOLE_MESSAGE_FORMAT = \
+    #     '[%(asctime)s] %(levelname)-8s %(name)s %(message)s'
 
     def __init__(self):
         dist = pkg_resources.get_distribution('aeromancer')
@@ -18,8 +22,18 @@ class Aeromancer(App):
             command_manager=CommandManager('aeromancer.cli'),
         )
 
-    # def initialize_app(self, argv):
-    #     self.log.debug('initialize_app')
+    def configure_logging(self):
+        super(Aeromancer, self).configure_logging()
+        if self.options.verbose_level < 2:
+            # Quiet the logger that talks about updating the database.
+            alembic_logger = logging.getLogger('alembic.migration')
+            alembic_logger.setLevel(logging.WARN)
+        return
+
+    def initialize_app(self, argv):
+        self.log.debug('updating database')
+        migrations.run_migrations()
+
 
     # def prepare_to_run_command(self, cmd):
     #     self.log.debug('prepare_to_run_command %s', cmd.__class__.__name__)
---
+++ aeromancer/db/migrations.py
@@ -0,0 +1,27 @@
+import logging
+
+from alembic.config import Config
+from alembic import command
+from alembic.environment import EnvironmentContext
+from alembic.script import ScriptDirectory
+from sqlalchemy import engine_from_config, pool
+
+
+LOG = logging.getLogger(__name__)
+
+
+def my_function(rev, context):
+    #LOG.info('rev = %s', rev)
+    print 'rev=%s' % (rev,)
+    return rev
+
+
+def run_migrations():
+    config = Config()
+    config.set_main_option("script_location", "aeromancer.db:alembic")
+    config.set_main_option("sqlalchemy.url", "sqlite:////Users/dhellmann/.aeromancer/aeromancer.db")
+    command.upgrade(config, 'head')
+
+
+if __name__ == '__main__':
+    run_migrations()
